908b9e7fef26f00601ad73124ec94e0fa4c873e4
kakuzu003-afk <kakuzu003@gmail.com>
2026-05-28 19:38:20 -0400

    Update bot_social.py

--- bot_social.py
+++ bot_social.py
@@ -831,4 +831,4 @@ if __name__ == '__main__':
     hilo_scheduler.daemon = True
     hilo_scheduler.start()
     puerto = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=puerto, debug=False, allow_unsafe_werkzeug=True)e)
+    socketio.run(app, host='0.0.0.0', port=puerto, debug=False)
